e8475a5d1baeceb0af3596942f4b4efb68b71203
kiler129 <grzegorz@noflash.pl>
2018-09-28 21:23:56 -0500

    bug #261: Improve SearchableEntityInterface docs

--- src/SearchableEntityInterface.php
+++ src/SearchableEntityInterface.php
@@ -7,7 +7,13 @@ interface SearchableEntityInterface
 {
     public function getIndexName();
 
+    /**
+     * @return array Serialized entity fields
+     */
     public function getSearchableArray();
 
+    /**
+     * @return string|int|array Serialized entity identifier
+     */
     public function getId();
 }
